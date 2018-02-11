d6a268d41d1bef1c4289416a675db00fbc915394
Fejcvk <tomaszfejcak@icloud.com>
2018-02-11 23:52:21 +0100

    final commit, app is working. Necessary fields are stored in dictionary which is filtered via LINQ

--- Proanthyocyanidin calculator/Form1.Designer.cs
+++ Proanthyocyanidin calculator/Form1.Designer.cs
@@ -28,12 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.loadDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1564, 32);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadDatabaseToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(94, 29);
+            this.toolStripDropDownButton1.Text = "Settings";
+            // 
+            // loadDatabaseToolStripMenuItem
+            // 
+            this.loadDatabaseToolStripMenuItem.Name = "loadDatabaseToolStripMenuItem";
+            this.loadDatabaseToolStripMenuItem.Size = new System.Drawing.Size(212, 30);
+            this.loadDatabaseToolStripMenuItem.Text = "Load database";
+            this.loadDatabaseToolStripMenuItem.Click += new System.EventHandler(this.loadDatabaseToolStripMenuItem_Click);
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip2.Location = new System.Drawing.Point(0, 32);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(1564, 25);
+            this.toolStrip2.TabIndex = 2;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 53);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(898, 790);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(966, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(273, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1245, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 36);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Filter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(971, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(281, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Type in name of product to filter the list";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1564, 1005);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.toolStrip2);
+            this.Controls.Add(this.toolStrip1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem loadDatabaseToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Proanthyocyanidin calculator/Form1.cs
+++ Proanthyocyanidin calculator/Form1.cs
@@ -7,14 +7,140 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace Proanthyocyanidin_calculator
 {
     public partial class Form1 : Form
     {
+        String pathToDb = null;
+        OleDbConnection con;
+        Dictionary<string, FoodObject> dictionary = new Dictionary<string, FoodObject>();
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void loadDatabaseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            String path ="";
+
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Access database files (*.accdb)|*.accdb;";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                path = file.FileName;
+                checkConnectionToDb(path);
+                pathToDb = path;
+            }
+            createObjectQuery();
+            foreach (KeyValuePair<string, FoodObject> entry in dictionary)
+            {
+                richTextBox1.Text += entry.Value.Name + " " + entry.Value.SumOfMers + "\n";
+            }
+            Console.WriteLine(path);
+        }
+
+        private void checkConnectionToDb(String path)
+        {
+            try
+            {
+                con = perfomConnection(path);
+                con.Open();
+                MessageBox.Show("Connected to db");
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex);
+            }
+        }
+        private OleDbConnection perfomConnection(String path)
+        {
+            OleDbConnection connection = new OleDbConnection();
+            connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Jet OLEDB:Database Password=MyDbPassword;";
+            return connection;
+        }
+
+
+        private void createObjectQuery()
+        {
+            con = perfomConnection(pathToDb);
+            con.Open();
+            OleDbDataReader reader = null;
+            var cmd = new OleDbCommand("SELECT * FROM PA_DAT;", con);
+            reader = cmd.ExecuteReader();
+            String prevId = "";
+            while (reader.Read())
+            {
+                string id = reader["NDB No"].ToString();
+                FoodObject food;
+
+                //jesli juz istnieje w slowniku obiekt wyciagam ze slownika
+                if(prevId.Equals(id))
+                {
+                    food = dictionary[id];
+                }
+                //jesli nie o robie nowu
+                else
+                {
+                    food = new FoodObject(id);
+                }
+                food.SumOfMers += Double.Parse(reader["Flav_Val"].ToString());
+                if (!dictionary.ContainsKey(id))
+                {
+                    OleDbDataReader reader2 = null;
+                    var cmd2 = new OleDbCommand("SELECT * FROM FOOD_DES WHERE FOOD_DES.[NDB No]='" + id + "'", con);
+                    reader2 = cmd2.ExecuteReader();
+                    while (reader2.Read())
+                    {
+                        food.Name = reader2["Long_Desc"].ToString();
+                        food.FoodGroupId = reader2["Long_Desc"].ToString();
+                        dictionary.Add(id, food);
+                    }
+                }
+                prevId = id;
+            }
+            con.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == null || textBox1.Text.Equals(""))
+            {
+                MessageBox.Show("Name of product cannot be blank!", "Error");
+            }
+            else
+            {
+                string name = textBox1.Text;
+                var r = from x in dictionary
+                        where x.Value.Name.ToLower().Contains(name.ToLower())
+                        select x;
+                richTextBox1.Clear();
+                foreach(KeyValuePair<string, FoodObject> obj in r)
+                {
+                    richTextBox1.Text += obj.Value.ToString() + "\n";
+                }
+            }
+        }
+    }
+    public class FoodObject
+    {
+        private string id;
+        private string name;
+        private string foodGroupId;
+        private double sumOfMers;
+        public FoodObject(string id)
+        {
+            this.id = id;
+        }
+        public String ToString()
+        {
+            String ret = this.name + " " + this.SumOfMers.ToString();
+            return ret;
+        }
+        public string Name { get => name; set => name = value; }
+        public string FoodGroupId { get => foodGroupId; set => foodGroupId = value; }
+        public double SumOfMers { get => sumOfMers; set => sumOfMers = value; }
     }
 }
